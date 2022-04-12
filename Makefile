07be2962158d421d2d952f146d3dbaf48e33e8db
Antonin Raffin <antonin.raffin@ensta.org>
2022-04-12 17:09:52 +0200

    Switch to setup.py

--- Makefile
+++ Makefile
@@ -1,25 +1,29 @@
 # Adapted from DLR-RM/stable-baselines3
-PACKAGE_NAME=eagerx_template
+PACKAGE_NAME=eagerx_ode
 
 SHELL=/bin/bash
-LINT_PATHS=${PACKAGE_NAME}/
+LINT_PATHS=${PACKAGE_NAME}/ *.py
 
 pytest:
 	bash ./scripts/run_tests.sh
 
 check-codestyle:
+	# Sort imports
+	isort --check ${LINT_PATHS}
 	# Reformat using black
-	poetry run black --check -l 127 ${LINT_PATHS}
+	black --check ${LINT_PATHS}
 
 codestyle:
+	# Sort imports
+	isort ${LINT_PATHS}
 	# Reformat using black
-	poetry run black -l 127 ${LINT_PATHS}
+	black ${LINT_PATHS}
 
 lint:
 	# stop the build if there are Python syntax errors or undefined names
 	# see https://lintlyci.github.io/Flake8Rules/
-	poetry run flake8 ${LINT_PATHS} --count --select=E9,F63,F7,F82 --show-source --statistics
+	flake8 ${LINT_PATHS} --count --select=E9,F63,F7,F82 --show-source --statistics
 	# exit-zero treats all errors as warnings.
-	poetry run flake8 ${LINT_PATHS} --count --exit-zero --statistics
+	flake8 ${LINT_PATHS} --count --exit-zero --statistics
 
 .PHONY: check-codestyle
---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name="eagerx_ode",
+    packages=[package for package in find_packages() if package.startswith("eagerx_ode")],
+    # package_data={"sb3_contrib": ["py.typed", "version.txt"]},
+    install_requires=["eagerx>=0.1.13"],
+    extras_require={
+        "tests": [
+            # Run tests and coverage
+            "pytest",
+            "pytest-cov",
+            # Type check
+            "pytype",
+            # Lint code
+            "flake8>=3.8",
+            # Find likely bugs
+            "flake8-bugbear",
+            # Sort imports
+            "isort>=5.0",
+            # Reformat
+            "black",
+        ],
+    },
+    description="",
+    author="Antonin Raffin",
+    url="",
+    author_email="antonin.raffin@dlr.de",
+    license="MIT",
+    long_description="",
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    python_requires=">=3.7",
+    # PyPI package information.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
